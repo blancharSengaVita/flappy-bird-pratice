6602c38603e243bc720998e92a31ddf172e73471
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-06-14 19:08:44 +0200

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require('laravel-mix');
+
+mix.ts('src/ts/Main.ts', 'js/main.js').sourceMaps()
+    .sass('src/scss/main.scss', 'css').sourceMaps()
+    .setPublicPath('dist').setResourceRoot('../')
+    .browserSync({
+        proxy: 'http://flappy-bird-pratice.test/',
+    });
